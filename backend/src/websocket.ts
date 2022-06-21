853e38eceb0f0f231f01f8d93a8e37df05c4d71d
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-06-20 21:38:11 -0300

    feat: add socket on

--- backend/src/websocket.ts
+++ backend/src/websocket.ts
@@ -1,6 +1,24 @@
 import { io } from "./server";
 
-io.on('connection', socket => {
-  console.log(socket.id);
+interface IDados {
+  socketId: string,
+  nome: string,
+  sala: string
+}
+
+const usuarios: IDados[] = [];
+
+io.on('connection', (socket) => {
   socket.emit('connection', null);
+  socket.on('sala_selecionada', (dados) => {
+    const { nome, sala } = dados;
+
+    socket.join(sala);
+
+    usuarios.push({
+      socketId: socket.id,
+      nome,
+      sala
+    });
+  });
 });
