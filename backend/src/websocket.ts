ab32b1ae30b6cb077142860c261b39b277fa8f0d
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-06-23 16:09:03 -0300

    feat: add socket emit and socket on mensagem - backend

--- backend/src/websocket.ts
+++ backend/src/websocket.ts
@@ -1,24 +1,52 @@
 import { io } from "./server";
 
-interface IDados {
+interface IDado {
   socketId: string,
   nome: string,
   sala: string
 }
 
-const usuarios: IDados[] = [];
+interface IMensagem {
+  sala: string,
+  usuario: string,
+  texto: string,
+  createdAt: Date
+}
+
+const usuarios: IDado[] = [];
+const mensagens: IMensagem[] = [];
 
 io.on('connection', (socket) => {
+
   socket.emit('connection', null);
+
   socket.on('sala_selecionada', (dados) => {
     const { nome, sala } = dados;
 
     socket.join(sala);
 
-    usuarios.push({
+    const usuarioNaSala = usuarios.find((usuario) => (
+      usuario.nome === nome && usuario.sala === sala));
+
+    if (usuarioNaSala) usuarioNaSala.socketId === socket.id;
+    else usuarios.push({
       socketId: socket.id,
       nome,
       sala
     });
+  });
+
+  socket.on('texto', (dados) => {
+    const usuario = usuarios.find((dadosDoUsuario) => dadosDoUsuario.socketId === socket.id);
+    
+    if (usuario) {
+      const { sala, nome } = usuario;
+
+      const mensagem: IMensagem = { sala, usuario: nome, texto: dados, createdAt: new Date() };
+
+      mensagens.push(mensagem);
+
+      io.to(sala).emit('mensagem', mensagem);
+    }
   });
 });
