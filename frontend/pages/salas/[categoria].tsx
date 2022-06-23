aa19f6823910ec9984490e7a0e6df213f9b6bc64
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-06-23 16:08:24 -0300

    feat: add socket emit and socket on message

--- frontend/pages/salas/[categoria].tsx
+++ frontend/pages/salas/[categoria].tsx
@@ -1,19 +1,33 @@
-import React, { useContext } from 'react';
+import React, { FormEvent, useContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import AppContext from '../../context/AppContext';
 import estilos from '../../styles/Sala.module.css';
 import SOCKET from '../../services/socket';
+import Mensagem from '../../components/Mensagem/Mensagem';
+
+interface IMensagem {
+  sala: string,
+  usuario: string,
+  texto: string,
+  createdAt: Date
+}
 
 const Sala = () => {
   const { nome } = useContext(AppContext);
   const router = useRouter()
+  const [texto, setTexto] = useState('');
+  const [mensagens, setMensagens] = useState<IMensagem[]>([]);
   const { categoria } = router.query
-
-  SOCKET.on('connection', () => {
-    console.log(`I'm connected with the back-end`);
-  });
-
+  
+  SOCKET.on('connection', () => console.log(`I'm connected with the back-end`));
   SOCKET.emit('sala_selecionada', { nome, sala: categoria });
+  SOCKET.on('mensagem', (dados) => setMensagens([...mensagens, dados]));
+  
+  const enviarTexto = (e: FormEvent) => {
+    e.preventDefault();
+    SOCKET.emit('texto', texto)
+    setTexto('');
+  }
 
   return (
     <div className={ estilos.paginaSala }>
@@ -23,8 +37,20 @@ const Sala = () => {
 
       <main className={ estilos.mainContainer }>
         <section>
-          <form>
-            <textarea rows={3} autoFocus placeholder="Digite aqui" />
+          {
+            mensagens.length > 0 && (
+              mensagens.map((mensagem, index) => (
+                <Mensagem dados={mensagem} key={`mensagem-${index + 1}`} />))
+            )
+          }
+          <form onSubmit={enviarTexto}>
+            <textarea
+              autoFocus
+              onChange={(e) => setTexto(e.target.value)}
+              placeholder="Digite aqui" 
+              rows={3}
+              value={texto}
+            />
             <button
               type="submit"
             >
